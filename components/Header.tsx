02277eaa70418bbabd5facb5fef92793825ffbd7
Desmond-Fon <desmondfon@yahoo.com>
2023-03-14 16:12:48 +0100

    added header

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return ( <div className="flex justify-between mt-[19px]">
+    <p className="text-[30px] leading-[46px] text-darkGray font-sora font-bold">Dez-folio</p>
+
+    <div>
+        <ul className="flex justify-between gap-8 font-mulish">
+            <li>Project</li>
+            <li>About me</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    </div> );
+}
+ 
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        backroundWhite : '#FFFFFFFF',
+        darkGray : ' #171A1FFF',
+        lightGray : ' #323842FF',
+        lightBlue: '#3EBDE0FF',
+        lightGreen : '#83AA19FF',
+        linkColor: ' #424955FF',
+        footerColor: ' #1D2128FF',
+        blueBackgroundColor: '#F2FAFDFF',
+        grayBackgroundColor : '#F6FCE8FF'
+      },
+      fontFamily: {
+        'sora': ['Sora', 'sans-serif'],
+        'mulish': ['Mulish', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
