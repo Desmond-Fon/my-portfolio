893299666e9a275239b80f5ef9e022c1aaa9c8d0
Desmond-Fon <desmondfon@yahoo.com>
2023-04-23 13:54:16 +0100

    added contents to the work section

--- components/Card.tsx
+++ components/Card.tsx
@@ -24,11 +24,11 @@ const Card = ({ description, site, code, image }: CardProps) => {
       onMouseOut={handleMouseOut}>
       {isHovering && (
         <div className="absolute bg-black/75 backdrop-sepia-0 text-white w-full h-full text-center flex justify-center items-center gap-4 flex-col px-6">
-          <p>{description}</p>
+          <p className="font-mulish">{description}</p>
           <div className="text-center flex justify-center items-center gap-4">
-            <Link href={site}><button className="w-[150px] h-[40px] text-[12px] bg-white text-black tracking-[4px] font-medium  hover:bg-red-600 hover:text-white">Live Site</button></Link>
+            <Link href={site} target="_blank"><button className="w-[150px] h-[40px] text-[12px] bg-white text-black tracking-[4px] font-medium  hover:bg-red-600 hover:text-white">Live Site</button></Link>
             {
-              code && <Link href={code || ''}><button className="w-[150px] h-[40px] text-[12px] border-[2px] border-white text-white tracking-[4px] font-medium hover:bg-black hover:text-white">See Code</button></Link>
+              code && <Link href={code || ''} target="_blank"><button className="w-[150px] h-[40px] text-[12px] border-[2px] border-white text-white tracking-[4px] font-medium hover:bg-black hover:text-white">See Code</button></Link>
             }
           </div>
 
--- components/Work.tsx
+++ components/Work.tsx
@@ -16,21 +16,21 @@ const Work = () => {
 
     <div className="flex justify-between flex-wrap">
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image='https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239281/ScreenShot_Tool_-20230405063538_asgt9z.png' />
+      <Card description="Implemented the front-end part of nHub Nigeria's official website. It was implemented using react and CSS." site='https://nhubnigeria.ng/' image='https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239281/ScreenShot_Tool_-20230405063538_asgt9z.png' />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' code='kj' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239293/Screenshot_from_2023-04-05_06-31-13_ryotfu.png" />
+      <Card description="Collaborated with some developers to develop a site where businesses can register and advertise their products and services. It was implemented using Next JS and Chakra UI for styling." site='https://kasuwangizo.ng/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239293/Screenshot_from_2023-04-05_06-31-13_ryotfu.png" />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image='https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239265/ScreenShot_Tool_-20230423093915_ttimlo.png' code='sd' />
+      <Card description="Currently working on an e-learning website headed by nHub Nigeria where users can create 'boxes', add participants, and also add learning content to the box. The progress of each participant can be viewed, as well as other information. It is built using React + Vite, and Chakra UI." site='https://pandora.nhubnigeria.ng/' image='https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239265/ScreenShot_Tool_-20230423093915_ttimlo.png' />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239570/spinc_mtrhmz.png" code='n' />
+      <Card description="Collaborated with a developer to create a website that allowed users to track their goods or service in transit. Each user needs a unique tracking ID to see the current progress location of their parcel. It was implemented using HTML, CSS, and vanilla javascript." site='https://spinc-crystal.netlify.app/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239570/spinc_mtrhmz.png" code='https://github.com/Desmond-Fon/Shipper-App' />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239743/movie_yp8fil.png" code="sd" />
+      <Card description="Created a website where users can check out and search for any kind of movie or TV series. Used an API from 'The Movie Database' to fetch the movie data. It was built using React and Tailwind CSS." site='https://movie-website-alpha.vercel.app/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239743/movie_yp8fil.png" code="https://github.com/Desmond-Fon/movie-website" />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682240599/rest_bhnrvl.png" code='df' />
+      <Card description="Built a website where a user can view all countries, search for a particular country, or filter countries by continent. A user can also see more information about a country by clicking on the country card. Used an API from 'REST Countries' to fetch the country data. It was built using React and Tailwind CSS." site='https://rest-country-search-app.netlify.app/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682240599/rest_bhnrvl.png" code='https://github.com/Desmond-Fon/rest-country-react' />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239934/rock_rqiftx.png" code='sd' />
+      <Card description="Developed a rock-paper-scissors game using React. It only allows a single player to play at a time, and it has a scoreboard that keeps a record of the current score of the player. It was built using React and tailwind CSS." site='https://desmond-fon.github.io/rock-paper-scissors-react/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682239934/rock_rqiftx.png" code='https://github.com/Desmond-Fon/rock-paper-scissors-react' />
 
-      <Card description="jsdkh sdfhsdkjf nsdfhljkdh sdfsldhj sdkljshf" site='sd' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682240918/color_mllalm.png" code='ds' />
+      <Card description="Created an RGB color guesser game that gives the user a color and then gives multiple options with different RGB color codes for the user to select from. A user can toggle between easy and hard levels. It was built using React and Tailwind CSS." site='https://rgb-color-picking-game.netlify.app/' image="https://res.cloudinary.com/dvikxcdh3/image/upload/v1682240918/color_mllalm.png" code='https://github.com/Desmond-Fon/color-picker' />
 
     </div>
 
